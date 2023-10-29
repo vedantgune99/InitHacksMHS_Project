90f29021bb26e0461a3dad12a64463fcb8b84606
Vedant Gune <vedantgune41@gmail.com>
2023-10-29 10:06:26 +0530

    score limited

--- MHS/views.py
+++ MHS/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from  Accounts.models import Profile
+from Accounts.models import Profile
 from config import emailID, password
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import smtplib
 import pickle
-
-
 
 
 # Create your views here.
@@ -18,7 +16,6 @@ def homepage(request):
 
 def about(request):
     return render(request, 'about.html')
-
 
 
 def contact(request):
@@ -42,18 +39,17 @@ def contact(request):
 
                                 Query Submitted Successfully!                
                                 ''')
-                        )
+                       )
         with smtplib.SMTP(host="smtp.gmail.com", port=587) as smtp:
             smtp.ehlo()
             smtp.starttls()
             smtp.login(emailID, password)
             smtp.send_message(message)
 
-        messages.add_message(request, messages.SUCCESS, 'Query submitted successfully!')
+        messages.add_message(request, messages.SUCCESS,
+                             'Query submitted successfully!')
 
     return render(request, 'contact.html')
-
-
 
 
 def health_test(request):
@@ -82,45 +78,41 @@ def health_test(request):
             q18 = request.POST.get('q18')
             q19 = request.POST.get('q19')
             q20 = request.POST.get('q20')
-            datapts = [age, gender, q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q11, q12, q13, q14, q15, q16, q17, q18, q19, q20]
+            datapts = [age, gender, q1, q2, q3, q4, q5, q6, q7, q8, q9,
+                       q10, q11, q12, q13, q14, q15, q16, q17, q18, q19, q20]
             data = [int(x) for x in datapts if x.isdigit()]
             score = data.count(1)
             score = score * 10
-            
-            
+
             if not data:
-                messages.add_message(request, messages.ERROR ,'Invalid input. Please enter numeric values.')
+                messages.add_message(
+                    request, messages.ERROR, 'Invalid input. Please enter numeric values.')
             else:
                 model_loaded = pickle.load(open('./MHSModel.pkl', 'rb'))
                 prediction = model_loaded.predict([data])
 
-                if prediction[0] == 1 and score >= 100:
-                    messages.add_message(request, messages.ERROR, f'Your test results are positive! Score : {score}')
-                    
+                if prediction[0] == 1 and score >= 70:
+                    messages.add_message(
+                        request, messages.ERROR, f'Your test results are positive! Score : {score}')
+
                     return redirect('dashboard')
-                    
+
                 else:
-                    messages.add_message(request, messages.SUCCESS, f'Your test results are negative! Score : {score}')
+                    messages.add_message(
+                        request, messages.SUCCESS, f'Your test results are negative! Score : {score}')
                     return redirect('home')
-                    
+
         else:
             return render(request, 'health_test.html')
 
     except Exception as e:
         return redirect('health_test')
 
-    
     return render(request, 'health_test.html')
-
-
-
 
 
 def chatbot(request):
     return render(request, 'chatbot.html')
-
-
-
 
 
 def video_conference(request):
@@ -138,13 +130,13 @@ def video_conference(request):
                             \nJoin here at + \ 
                             \nRoom Link : http://127.0.0.1:8000/video_conference/?roomID=100                 
                             """)
-                    )
+                   )
     with smtplib.SMTP(host="smtp.gmail.com", port=587) as smtp:
         smtp.ehlo()
         smtp.starttls()
         smtp.login(emailID, password)
         smtp.send_message(message)
 
-    messages.add_message(request, messages.SUCCESS, 'Connecting with Specialists')
-    return render(request, 'video_conferencing.html', {'name':request.user})
-
+    messages.add_message(request, messages.SUCCESS,
+                         'Connecting with Specialists')
+    return render(request, 'video_conferencing.html', {'name': request.user})
